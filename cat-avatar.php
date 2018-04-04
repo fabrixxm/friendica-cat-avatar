1d80592a2bf817fba5af57e103a04cf6ba01ee87
Fabio <f.comuni@creative-group.it>
2018-04-04 09:11:37 +0200

    first version

---
+++ cat-avatar.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Name: Cat Avatar Generator
+ * Description: Generate a default avatar based on David Revoy's cat-avatar-generator https://framagit.org/Deevad/cat-avatar-generator  
+ * Version: 1.1
+ * Author: Fabio <https://kirgroup.com/profile/fabrixxm>
+ */
+use Friendica\Core\Addon;
+use Friendica\Core\Config;
+use Friendica\Core\L10n;
+
+/**
+ * Installs the addon hook
+ */
+function catavatar_install() {
+	Addon::registerHook('avatar_lookup', 'addon/cat-avatar/cat-avatar.php', 'catavatar_lookup');
+
+	logger("registered cat-avatar in avatar_lookup hook");
+}
+
+/**
+ * Removes the addon hook
+ */
+function catavatar_uninstall() {
+	Addon::unregisterHook('avatar_lookup', 'addon/catavatar/catavatar.php', 'catavatar_lookup');
+
+	logger("unregistered cat-avatar in avatar_lookup hook");
+}
+
+/**
+ * Looks up the avatar at catavatar.com and returns the URL.
+ *
+ * @param $a array
+ * @param &$b array
+ */
+function catavatar_lookup($a, &$b) {
+	$hash = md5(trim(strtolower($b['email'])));
+
+	$url = $a->get_baseurl().'/addon/cat-avatar/cat-avatar-generator/cat-avatar-generator.php?seed=' .$hash;
+
+	$b['url'] = $url;
+	$b['success'] = true;
+}
+
